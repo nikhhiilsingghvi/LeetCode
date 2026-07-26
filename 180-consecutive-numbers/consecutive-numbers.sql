8a9714ae2fb12250e0eb3dbdb3afa851021ef2dd
Nikhil Singhvi <nikhilj617310@gmail.com>
2026-07-26 13:31:06 +0530

    Time: 274 ms (45.44%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,8 +1,8 @@
-SELECT DISTINCT num AS ConsecutiveNums
-FROM (SELECT num,
-        LAG(num, 1) OVER (ORDER BY id) AS prev1,
-        LAG(num, 2) OVER (ORDER BY id) AS prev2
-    FROM Logs
-) t
-WHERE num = prev1
-AND num = prev2;
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2
+    ON l2.id = l1.id + 1
+JOIN Logs l3
+    ON l3.id = l1.id + 2
+WHERE l1.num = l2.num
+  AND l2.num = l3.num;
